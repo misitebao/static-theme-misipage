7aa98c776208208b99f96c73249edcbf18a8751a
misitebao <i@misitebao.com>
2022-07-21 22:29:09 +0800

    feat: upgrade vite and switch vue to petite-vue

--- src/pages/about/main.js
+++ src/pages/about/main.js
@@ -1,29 +1,5 @@
-import { createApp, ref } from 'vue/dist/vue.esm-bundler.js';
+import { createApp } from 'petite-vue';
 
 import './styles/main.scss';
 
-import useHeader from '@/hooks/useHeader.js';
-import useFooter from '@/hooks/useFooter.js';
-
-createApp({
-  // 设置Vue模板文件渲染标记符,主要是为了避免和渲染引擎标记冲突
-  compilerOptions: {
-    delimiters: ['{[', ']}'],
-    comments: true,
-  },
-  components: {
-    // App,
-  },
-  setup() {
-    const { name: headerName } = useHeader();
-    const { name: footerName } = useFooter();
-
-    const name = ref('about page');
-
-    return {
-      headerName,
-      footerName,
-      name,
-    };
-  },
-}).mount('#app');
+createApp({}).mount('#app');
