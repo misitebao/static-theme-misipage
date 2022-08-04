4c69342128eed021364bd7a71df091013b3b690b
Misite Bao <i@misitebao.com>
2022-08-04 19:37:26 +0800

    feat: remove unused dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { fileURLToPath, URL } from 'url';
 
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
 import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
     createHtmlPlugin({
       minify: false,
       pages: [
