679798d54f62e0bb822d85947b9b51fd21c4d5e9
Misite Bao <i@misitebao.com>
2022-10-13 19:41:59 +0800

    feat: sync the theme of the comment component

--- src/hooks/theme.js
+++ src/hooks/theme.js
@@ -8,12 +8,32 @@ export default () => ({
       (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
     ) {
       this.theme = 'dark';
+      changeGiscusTheme('dark');
     } else {
       this.theme = 'light';
+      changeGiscusTheme('light');
     }
   },
   change(theme) {
     this.theme = theme;
     localStorage.theme = theme;
+    changeGiscusTheme(theme);
   },
 });
+
+function changeGiscusTheme(theme) {
+  const iframe = document.querySelector('iframe.giscus-frame');
+  console.log(iframe);
+  if (!iframe) return;
+
+  iframe.contentWindow.postMessage(
+    {
+      giscus: {
+        setConfig: {
+          theme: theme,
+        },
+      },
+    },
+    'https://giscus.app'
+  );
+}
